ae8c946b970a38a2680ab6e14387390b5809d65d
weichuan <weichuan.tran@gmail.com>
2018-05-23 01:30:10 +0200

    jquery intro

---
+++ exo1/jquery-3.3.1.js
@@ -0,0 +1,63 @@
+$(document).ready(function(e) {
+  $(".d")
+    .parent()
+    .addClass('g')
+})
+
+
+// $(function(e) {
+//   $('p', '.a')
+//     .children()
+//     .addClass("g")
+// });
+
+
+
+//   $('p.a')
+//     .css("font-style","italic")
+//     .css("font-size", "3em")
+//     .addClass("g");
+// });
+
+
+// $(function(e) {
+//   $('p.a', this).css("font-style","italic");
+// });
+
+
+// $(function(e) {
+//   $('p.a', '.b').css("font-style","italic");
+// });
+
+
+
+// $(function(e) {
+//   $('p:even').css("font-style","italic");
+// });
+
+
+// les 4 fonctions font la meme chose
+// juste ecris en plus raccourcis...
+
+
+// $(function(e) {
+//   $('p:contains("Class"):not(.a)').css("font-style","italic");
+// });
+
+// tout les p contenant le mot Class et on ne veut pas prendre en compte les classes a
+
+
+// $().ready(function(e){
+//   $('#b').css("font-style","italic");
+// });
+
+
+// $(document).ready(function(e){
+//   $('#b').css("font-style","italic");
+// });
+
+
+
+// window.onload = function() {
+//   $('#b').css("font-style","italic");
+// };

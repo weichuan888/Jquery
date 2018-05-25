8f92e4b6ee3544b39a63df356c8d7b22c5d42466
weichuan <weichuan888tran@gmail.com>
2018-05-25 14:40:15 +0200

    hover ok

--- exo2/jquery-3.3.1.js
+++ exo2/jquery-3.3.1.js
@@ -1,15 +1,36 @@
+// $(document).ready(function(e){
+//     $("h1").text('marsuuuupilami est de retour!')
+//     $('li').text(
+//         function(index){
+//             return 'choix =====>' + (index + 1)
+//         }
+//     );
+// });
+
+// $(document).ready(function(e){
+//     $('p').hide('fast')
+//     $('h2').on('click', function(e){
+//         $('p').slideToggle('slow')
+//     })
+// });
+
+// $(document).ready(function(e){
+//     $('.test').hover(
+//         function(){
+//             $(this).append('<p> Cliquer pour ajouter une navette </p>');  
+//         },
+//         function() {
+//             $(this).find()
+//         }
+//     )    
+// })
 $(document).ready(function(e){
-    $("h1").text('marsuuuupilami est de retour!')
-    $('li').text(
-        function(index){
-            return 'choix =====>' + (index + 1)
-        }
-    );
+    $('.test').hover(
+        function(){
+            $(this).append("<p> Cliquer pour ajouter une nouvelle navette </p>");
+        },
+        function(){
+            $(this).find('p').remove();
+        });    
 });
 
-$(document).ready(function(e){
-    $('p').hide('fast')
-    $('h2').on('click', function(e){
-        $('p').slideToggle('slow')
-    })
-});
